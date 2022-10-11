0eb5b619a0c6d90371d4d58922684b3a42650137
Bram Zijp <bramzijpcode@gmail.com>
2022-10-12 01:52:09 +0200

    chore: fake change

--- libs/core/README.md
+++ libs/core/README.md
@@ -7,3 +7,5 @@ Run `nx test core` to execute the unit tests via [Jest](https://jestjs.io).
 ## Running lint
 
 Run `nx lint core` to execute the lint via [ESLint](https://eslint.org/).
+
+hello
--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/worker';
 export * from './lib/createMock';
 export { loadFromStorage, saveToStorage } from './lib/state';
 export type { MocksState } from './lib/state';
+
+// hello
