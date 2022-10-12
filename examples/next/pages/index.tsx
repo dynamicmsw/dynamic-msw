30b1759a2f0cd2f58cc79c444e8620741b8fa383
Bram-Zijp <bramzijp1@gmail.com>
2022-10-12 02:23:12 +0200

    chore: cleanup next example (#21)

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -11,37 +11,21 @@ export function Index() {
     ) {
       const {
         exampleEndpoint,
+        exampleMock,
         //eslint-disable-next-line @typescript-eslint/no-var-requires
       } = require('@dynamic-msw/mock-example');
 
       fetch(exampleEndpoint)
         .then((res) => res.json())
         .then((data) => setData(data));
-    }
-  }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (
-        typeof window !== 'undefined' &&
-        (process.env.NODE_ENV === 'development' ||
-          process.env.NODE_ENV === 'test')
-      ) {
-        const {
-          exampleMock,
-          exampleEndpoint,
-          //eslint-disable-next-line @typescript-eslint/no-var-requires
-        } = require('@dynamic-msw/mock-example');
-
+      setTimeout(() => {
         exampleMock.updateMock({ success: false });
-
-        if (typeof window !== 'undefined') {
-          fetch(exampleEndpoint)
-            .then((res) => res.json())
-            .then((data) => setData(data));
-        }
-      }
-    }, 3500);
+        fetch(exampleEndpoint)
+          .then((res) => res.json())
+          .then((data) => setData(data));
+      }, 3500);
+    }
   }, []);
 
   return <div data-testid="test-mocked-data">{data?.success}</div>;
