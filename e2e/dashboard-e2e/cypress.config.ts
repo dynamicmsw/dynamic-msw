c9a9f1ab8182ab172b0326a3cfac1ccf4f644a32
Bram Zijp <bramzijpcode@gmail.com>
2022-10-16 14:35:32 +0200

    refactor: make build cache command multiline

--- e2e/dashboard-e2e/cypress.config.ts
+++ e2e/dashboard-e2e/cypress.config.ts
@@ -1,7 +1,11 @@
 import { nxE2EStorybookPreset } from '@nrwl/storybook/presets/cypress';
 import { defineConfig } from 'cypress';
 
+throw Error(
+  'check process.env.CI var: ' + (process.env.CI === 'true').toString()
+);
 export default defineConfig({
   e2e: { ...nxE2EStorybookPreset(__dirname), retries: 1 },
+  // TODO: test this
   videoUploadOnPasses: process.env.CI === 'true',
 });
