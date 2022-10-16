afd3de671df026290950756eaea1727605e59472
Bram Zijp <bramzijpcode@gmail.com>
2022-10-16 18:55:20 +0200

    feat: add open app page function

--- libs/open-app-page/src/lib/open-app-page.ts
+++ libs/open-app-page/src/lib/open-app-page.ts
@@ -1,3 +1,27 @@
-export function openAppPage(): string {
-  return 'open-app-page';
+import openPage from 'open';
+import type { Options } from 'open';
+import waitOnPageResolve from 'wait-on';
+import type { WaitOnOptions } from 'wait-on';
+
+interface OpenAppPageArg {
+  waitForAppPageURL: string;
+  openAppURL: string;
+  waitForOptions: WaitOnOptions;
+  openAppOptions: Options;
 }
+
+export const openAppPage = async ({
+  waitForAppPageURL,
+  openAppURL,
+  waitForOptions,
+  openAppOptions,
+}: OpenAppPageArg) =>
+  waitOnPageResolve({
+    ...waitForOptions,
+    resources: [waitForAppPageURL, ...(waitForOptions.resources || [])],
+  })
+    .then(() => openPage(openAppURL, openAppOptions))
+    .catch((err: Error) => {
+      console.error('waitForAppPageURL error:');
+      throw err;
+    });
