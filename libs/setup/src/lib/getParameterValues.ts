7893830eab8666a63ca0fd8e58490eb59dca8e68
Bram-Zijp <bramzijpcode@gmail.com>
2024-03-03 13:27:41 +0100

    fix(setup): use nullish coalescing for param value

--- libs/setup/src/lib/getParameterValues.ts
+++ libs/setup/src/lib/getParameterValues.ts
@@ -3,10 +3,12 @@ import { CreateMockEntity } from '@dynamic-msw/core';
 export function getParameterValues(configureMockEntity: CreateMockEntity) {
   if (!configureMockEntity.parameters) return {};
   return Object.entries(configureMockEntity.parameters).reduce(
-    (acc, [key, value]) => ({
-      ...acc,
-      [key]: value.currentValue || value.defaultValue || null,
-    }),
+    (acc, [key, value]) => {
+      return {
+        ...acc,
+        [key]: value.currentValue ?? value.defaultValue ?? null,
+      };
+    },
     {}
   );
 }
